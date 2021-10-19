4d2dfe7f22926afc1c1b41e34e8ee4df2d0184b7
Ubuntu <mercurial.universal@gmail.com>
2021-10-19 15:36:14 +0000

    .. -_- ..

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ provider "openstack" {
   auth_url    = "https://east-1.cloud.snic.se:5000/v3"
 }
 
+# key 
+resource "openstack_compute_keypair_v2" "my-cloud-key" {
+  name       = "stef-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgjz0c2tDJbVcgULgJR6BAyYI0rfQBk67tpR7cWyIPyd467j66GkXEwUhZRoEecGoLNqYLYyHcalV49BpvNrxif0oJxGNu/sUn9i1YLArBFDdJ0C9L0BbwK9p0hQkDJa4hEepj2R5o6W7fro55ueMIyJCJVJZk/3H0TIl5PYPxCArZRuiXw3QW4W2/MvnY/5vRydgrqv9TxSBLBTBvFfBjPHv3h9wxsh0TVRI1AIDXF/9W31di4uDtu68AavaDM/XrrfrFNPO0bVHxjCdZJi4nhM2+gJkRqdkZASl/VQUVuFs68bYaDW7IK3bTsqGLkkPu3uBrXPtZybMyhzZib1Bp"
+}
+
 # Fetch floating IPs for the two VMs
 resource "openstack_networking_floatingip_v2" "floating-ips" {
   pool = "Public External IPv4 Network"
@@ -24,13 +30,15 @@ resource "openstack_compute_instance_v2" "g5-flower-server-b" {
   image_name      = "Ubuntu 18.04"
   image_id        = "0b7f5fb5-a25c-48b6-8578-06dbfa160723"
   flavor_name     = "ssc.xsmall"
-  key_pair        = var.key_pair
+  key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
   security_groups = ["default", "group-5", "stefanos_sec_group"]
+
 
   network {
     name = "UPPMAX 2021/1-5 Internal IPv4 Network"
   }
 }
+
 
 # Give server a floating IP and upload files
 resource "openstack_compute_floatingip_associate_v2" "server-ip-associate" {
@@ -61,22 +69,12 @@ resource "openstack_compute_instance_v2" "g5-flower-client-b" {
   image_name      = "Ubuntu 18.04"
   image_id        = "0b7f5fb5-a25c-48b6-8578-06dbfa160723"
   flavor_name     = "ssc.xsmall"
-  key_pair        = var.key_pair
+  key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
   security_groups = ["default", "group-5", "stefanos_sec_group"]
   depends_on = [
     openstack_compute_instance_v2.g5-flower-server-b
   ]
-  #user_data = <<-EOF
-  #  #cloud-config
-  #  write_files:
-  #    - path: /home/ubuntu/config.py
-  #      owner: ubuntu:ubuntu
-  #      content: |
-  #        server_ip = "${openstack_compute_instance_v2.g5-flower-server-b.access_ip_v4}"
-  #  runcmd:
-  #    - sudo bash
-  #    - chown ubuntu:ubuntu /home/ubuntu
-  #EOF
+
 
   #personality {
   #  content = "server_ip = '${openstack_compute_instance_v2.g5-flower-server-b.access_ip_v4}'"
